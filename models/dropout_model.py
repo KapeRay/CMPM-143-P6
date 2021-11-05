d4eb7cc7f983aa1ba1b6e2461cf4a3cca69bf878
jontyjlewis <72323053+jontyjlewis@users.noreply.github.com>
2021-11-04 22:58:06 -0700

    dropout done

--- models/dropout_model.py
+++ models/dropout_model.py
@@ -1,9 +1,30 @@
 from tensorflow.keras import layers
 from tensorflow.keras import models
 from tensorflow.keras import optimizers
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 
 # Define your dropout model here
 model = models.Sequential()
+
+model.add(Conv2D(16, kernel_size=3, activation='relu', input_shape=(150, 150, 3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, kernel_size=3, activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, kernel_size=3, activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(128, kernel_size=3, activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+
+model.add(Dropout(rate=0.5))
+
+model.add(Dense(10, activation='relu'))
+
+model.add(Dense(1, activation='sigmoid'))
 
 # Then, call model.compile()
 model.compile(
@@ -11,6 +32,8 @@ model.compile(
     optimizer=optimizers.RMSprop(lr=1e-4),
     metrics=['acc']
 )
+
+model.summary()
 
 # Train this compiled model by modifying basic_train 
 # to import this model, then run:
--- train.py
+++ train.py
@@ -2,20 +2,25 @@ import matplotlib.pyplot as plt
 from tensorflow import keras
 # TODO Student: update basic_model -> dropout_model to change which model gets trained
 # from p6_deep_learning.models.basic_model import model
-from models.basic_model import model
+
+# from models.basic_model import model
+from models.dropout_model import model
+
 from preprocess import train_generator, validation_generator
 
 # Train the model defined in basic_model.py
 history = model.fit_generator(
     train_generator,
-    steps_per_epoch = 100,
-    epochs = 30,
-    validation_data = validation_generator,
-    validation_steps = 50)
+    steps_per_epoch=100,
+    epochs=30,
+    validation_data=validation_generator,
+    validation_steps=50)
 
 # Save the model weights
 # Change the name of this file to avoid overwriting previously trained models
-model.save('cats_and_dogs_small_1.h5')
+# model.save('cats_and_dogs_small_1.h5')
+model.save('cats_and_dogs_small_2.h5')
+
 
 # Plot the  loss and accuracy over the training run
 def plot(history):
@@ -23,7 +28,7 @@ def plot(history):
     val_acc = history.history['val_acc']
     loss = history.history['loss']
     val_loss = history.history['val_loss']
-    
+
     epochs = range(1, len(acc) + 1)
     plt.plot(epochs, acc, 'bo', label='Training acc')
     plt.plot(epochs, val_acc, 'b', label='Validation acc')
@@ -35,5 +40,6 @@ def plot(history):
     plt.title('Training and validation loss')
     plt.legend()
     plt.show()
+
 
 plot(history)
