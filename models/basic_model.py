8e33859288f426353382e6eb2cf97699293ed04f
jontyjlewis <72323053+jontyjlewis@users.noreply.github.com>
2021-11-04 20:57:24 -0700

    Part 4 done

--- models/basic_model.py
+++ models/basic_model.py
@@ -1,10 +1,27 @@
 from tensorflow.keras import layers
 from tensorflow.keras import models
 from tensorflow.keras import optimizers
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 
 # Define a sequential model here
 model = models.Sequential()
 # add more layers to the model...
+model.add(Conv2D(16, kernel_size=3, activation='relu', input_shape=(150, 150, 3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, kernel_size=3, activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, kernel_size=3, activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(128, kernel_size=3, activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(10, activation='relu'))
+
+model.add(Dense(5, activation='sigmoid'))
 
 # Then, call model.compile()
 model.compile(
@@ -12,6 +29,9 @@ model.compile(
     optimizer=optimizers.RMSprop(lr=1e-4),
     metrics=['acc']
 )
+
+# Shows the model summary
+model.summary()
 
 # Finally, train this compiled model by running:
 # python train.py
